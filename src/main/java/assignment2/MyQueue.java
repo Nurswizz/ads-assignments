78a7b440de6b7d5a8ba7c31b153320c723258f51
Nurswizz <nur646132@gmail.com>
2026-04-12 21:07:28 +0500

    feat: myqueue

---
+++ src/main/java/assignment2/MyQueue.java
@@ -0,0 +1,34 @@
+package assignment2;
+
+import assignment2.interfaces.MyList;
+
+public class MyQueue<T> {
+
+    private MyList<T> list;
+
+    public MyQueue() {
+        list = new MyLinkedList<>();
+    }
+
+    public void enqueue(T item) {
+        list.addLast(item);
+    }
+
+    public T dequeue() {
+        T value = list.getFirst();
+        list.removeFirst();
+        return value;
+    }
+
+    public T peek() {
+        return list.getFirst();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
